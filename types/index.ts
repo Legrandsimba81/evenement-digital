a250c2525aabd4caebfe54320c06903793f29be2
Legrand simba <legrandsimba81@gmail.com>
2026-06-26 03:08:42 +0200

    fix: suppression des imports Prisma dans types/index.ts (utilisation de any)

--- types/index.ts
+++ types/index.ts
@@ -1,22 +1,5 @@
 // types/index.ts
-import { Prisma } from '@prisma/client'
-
-// Utilisation des types génériques de Prisma pour récupérer les modèles
-type Event = Prisma.EventGetPayload<{}>
-type User = Prisma.UserGetPayload<{}>
-type Guest = Prisma.GuestGetPayload<{}>
-type Message = Prisma.MessageGetPayload<{}>
-
-// Définition des types avec relations
-export type EventWithRelations = Event & {
-  user: User
-  guests: Guest[]
-  messages: Message[]
-}
-
-export type UserWithEvents = User & {
-  events: Event[]
-}
-
+export type EventWithRelations = any
+export type UserWithEvents = any
 export type Role = "USER" | "ADMIN"
 export type EventType = "ANNIVERSAIRE" | "MARIAGE" | "SOUTENANCE" | "AUTRE"
