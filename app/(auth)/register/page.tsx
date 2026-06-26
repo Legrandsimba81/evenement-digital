c38d58c17acbba5231ef0f052722bab241e58e2e
Legrand simba <legrandsimba81@gmail.com>
2026-06-26 04:10:07 +0200

    fix: ajout de force-dynamic sur la page register

--- app/(auth)/register/page.tsx
+++ app/(auth)/register/page.tsx
@@ -1,5 +1,7 @@
 import AuthForm from "@/components/AuthForm";
 
+export const dynamic = 'force-dynamic'; // 👈 Ajoutez cette ligne
+
 export default function RegisterPage() {
   return (
     <div className="min-h-screen flex items-center justify-center bg-gray-50 p-4">
